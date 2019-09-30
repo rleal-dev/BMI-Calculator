9e31341aa2cab3668618492cfe13f644f6236c3b
Rogerio Leal <rogerioleal1@gmail.com>
2019-09-30 12:35:42 -0300

    Initial Commit - BMI Calculator

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Text, View } from 'react-native'
+
+function App() {
+    return (
+        <View>
+            <Text>Bmi Calculator</Text>
+        </View>
+    )
+}
+
+export default App
